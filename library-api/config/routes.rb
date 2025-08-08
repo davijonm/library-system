36bda7c1656bd09554e80e55f989f7cd36c3c8a7
Davi Mendes <davi.mendes@castgroup.com.br>
2025-08-08 11:33:00 -0300

    create routes

--- library-api/config/routes.rb
+++ library-api/config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # authentication routes
+  post '/register', to: 'auth#register'
+  post '/login', to: 'auth#login'
+  post '/logout', to: 'auth#logout'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # books routes
+  resources :books, only: [:index, :show, :create, :update, :destroy] do
+    collection do
+      get :search
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # borrowings routes
+  resources :borrowings, only: [:index, :show, :create] do
+    member do
+      patch :return_book
+    end
+    collection do
+      get :dashboard
+      get :overdue_members
+    end
+  end
 end
