9c4dce77040d273a3d7ba3b628416335bf4481ee
Davi Mendes <davi.mendes@castgroup.com.br>
2025-08-08 11:00:42 -0300

    create aut methods

--- library-api/app/controllers/application_controller.rb
+++ library-api/app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  
+  before_action :authenticate_user
+  
+  private
+  
+  def authenticate_user
+    @current_user = User.find(decoded_token['user_id']) if decoded_token
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Invalid token' }, status: :unauthorized
+  end
+  
+  def current_user
+    @current_user
+  end
+  
+  def decoded_token
+    @decoded_token ||= begin
+      token = extract_token_from_header
+      JwtService.decode(token) if token
+    end
+  end
+  
+  def extract_token_from_header
+    authorization_header = request.headers['Authorization']
+    return nil unless authorization_header
+    
+    token = authorization_header.split(' ').last
+    token if token.present?
+  end
 end
