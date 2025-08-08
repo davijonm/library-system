c0b9d86246b56ba5856971185056aaa823e9b7c9
Davi Mendes <davi.mendes@castgroup.com.br>
2025-08-08 12:16:32 -0300

    create tests and factory for user

--- library-api/spec/factories/users.rb
+++ library-api/spec/factories/users.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
-    role { "MyString" }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { "password123" }
+    role { "member" }
+
+    trait :librarian do
+      role { "librarian" }
+    end
+
+    trait :member do
+      role { "member" }
+    end
   end
 end
